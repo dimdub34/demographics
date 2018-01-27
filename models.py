1c7cb22c515d1d48448793d0c814edf7d6dc236c
Dimitri DUBOIS <dimitri.dubois@umontpellier.fr>
2018-01-27 17:42:03 +0100

    Replace verbose_name by label Add gettext to nationalities Fill the tests.py file

--- models.py
+++ models.py
@@ -29,17 +29,17 @@ class Group(BaseGroup):
 class Player(BasePlayer):
 
     age = models.IntegerField(
-        verbose_name=gettext('What is your age?'),
+        label=gettext('What is your age?'),
         min=13, max=125)
 
     gender = models.IntegerField(
         choices=[(0, gettext('Female')), (1, gettext('Male'))],
-        verbose_name=gettext('What is your gender?'),
+        label=gettext('What is your gender?'),
         widget=widgets.RadioSelectHorizontal)
 
     student = models.IntegerField(
         choices=[(0, gettext('No')), (1, gettext('Yes'))],
-        verbose_name=gettext("Are you a student?"),
+        label=gettext("Are you a student?"),
         widget=widgets.RadioSelectHorizontal)
 
     student_level = models.IntegerField(
@@ -48,20 +48,25 @@ class Player(BasePlayer):
         blank=True)
 
     student_discipline = models.StringField(
-        choices=["Administration", "Archeology", "Biology", "Buisiness school",
-                 "Chemistry", "Computer science", "Economics","Education",
-                 "Law", "Management", "Nursing school", "Engineer",
-                 "Geography", "History", "Lettres", "Mathematics", "Medicine",
-                 "Music", "Pharmacy", "Philosophy", "Physics", "Politics",
-                 "Sociology", "Sport", "Not in the list"],
-        verbose_name=gettext("What are you studying?"), blank=True)
+        choices=[
+            gettext("Administration"), gettext("Archeology"), gettext("Biology"),
+            gettext("Buisiness school"), gettext("Chemistry"),
+            gettext("Computer science"), gettext("Economics"),
+            gettext("Education"), gettext("Law"), gettext("Management"),
+            gettext("Nursing school"), gettext("Engineer"), gettext("Geography"),
+            gettext("History"), gettext("Lettres"), gettext("Mathematics"),
+            gettext("Medicine"), gettext("Music"), gettext("Pharmacy"),
+            gettext("Philosophy"), gettext("Physics"), gettext("Politics"),
+            gettext("Sociology"), gettext("Sport"), gettext("Not in the list")
+        ],
+        label=gettext("What are you studying?"), blank=True)
 
     sport = models.IntegerField(
         choices=[(0, gettext('No')), (1, gettext('Yes'))],
-        verbose_name=gettext("Do you pratice (regularly) some sport?"),
+        label=gettext("Do you pratice (regularly) some sport?"),
         widget=widgets.RadioSelectHorizontal)
 
     experience = models.IntegerField(
         choices=[(0, gettext('No')), (1, gettext('Yes'))],
-        verbose_name=gettext("Have you ever participated in an experiment?"),
+        label=gettext("Have you ever participated in an experiment?"),
         widget=widgets.RadioSelectHorizontal)
--- tests.py
+++ tests.py
@@ -1,11 +1,22 @@
-from otree.api import Currency as c, currency_range
+from otree.api import Currency as c, currency_range, Submission
 from . import pages
 from ._builtin import Bot
 from .models import Constants
+import random
+from django.utils.translation import gettext
 
 
 class PlayerBot(Bot):
 
     def play_round(self):
-        yield (pages.MyPage)
-        yield (pages.Results)
+        yield (pages.Demographic,
+               {
+                   "age": random.randint(15, 90),
+                   "gender": random.randint(0, 1),
+                   "student": random.randint(0, 1),
+                   "student_level": random.randint(0, 2),
+                   "student_discipline": gettext("Economics"),
+                   "sport": random.randint(0, 1),
+                   "experience": random.randint(0, 1)
+               })
+        yield Submission(pages.End, check_html=False)
